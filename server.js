e245bd82f5bc99a830da6b458bb4b32b3b461e48
yoanybejarano <yoanybejarano@gmail.com>
2018-06-21 22:39:00 -0500

    Se implementaron modelos y configuraciones para la conexion a MongoDB

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    mongoose = require('mongoose');
 
 app.set('port', process.env.PORT || 3000);
 
 config = require('./configure'),
     app = config(app);
-    
+
+mongoose.connect('mongodb://localhost/imgPloadr');
+mongoose.connection.on('open', function () {
+    console.log('Mongoose connected.');
+});
+
 app.get('/', function (req, res) {
     res.send('Hello World');
 });
